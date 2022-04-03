aa606d482d90fae7d759fd99938a90dedbb549c2
Hacene Sadoudi <hacene.sadoudi@alumni.univ-avignon.fr>
2022-04-03 18:55:58 +0200

    docs(init):  Add docs

--- matchstick.php
+++ matchstick.php
@@ -1,3 +1,12 @@
+/**
+ * Initialize the game board
+ * 
+ * @param  array  $board   : empty game board array
+ * @param  int    $nb_line : number of board lines
+ * 
+ * @author Hacene Sadoudi <sadoudi2019@gmail.com>
+ * @return array $board
+ */
 function init($board, $nb_line) {
     $rows = 1;
     for ($i = 0; $i < $nb_line; $i++) {
