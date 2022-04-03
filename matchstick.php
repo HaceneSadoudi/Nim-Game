a806e756defccf21e62fb82a93b5d8c82f720c22
Hacene Sadoudi <hacene.sadoudi@alumni.univ-avignon.fr>
2022-04-03 19:02:36 +0200

    feat: Display the game board in the console
    A match-stick is represented by a pipeline "|"

--- matchstick.php
+++ matchstick.php
@@ -16,6 +16,24 @@ function init($board, $nb_line) {
     return $board;
 }
 
+function display($board, $width) {
+    for ($j = 0; $j < $width; $j++) color(rand(9, 15), "*");
+    echo "\n";
+    for ($i = 0; $i < sizeof($board); $i++) {
+
+        $mid = (int)(($width - 2) / 2) - $i;
+        color(rand(9, 15), "*");
+        for ($j = 0; $j < $width - 2; $j++) {
+            if ($j >=  $mid && $j < $mid + $board[$i]) color(rand(9, 15), "|");
+            else echo " ";
+        }
+        color(rand(9, 15), "*");
+        echo "\n";
+    }
+    for ($j = 0; $j < $width; $j++) color(rand(9, 15), "*");
+    echo "\n";
+}
+
 /**
  * Print a text with a color
  * 
